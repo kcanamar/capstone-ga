fbe3fdbbdd6a74f4e1b136f1c879de71de5fb0bd
Kyle Canamar <canamar.kyle@gmail.com>
2022-08-07 22:06:56 -0700

    creates base layout

--- components/Layout.js
+++ components/Layout.js
@@ -8,9 +8,11 @@ export default function Layout({ children, home }) {
         <div>
             <header></header>
             <main>
-                <Nav />
-                {children}
-                <Socials />
+                <Nav className="top-0"/>
+                <div className="flex justify-center h-screen m-auto" style={{height: 100 + 'vh'}}>
+                    {children}
+                </div>
+                <Socials className="bottom-0"/>
             </main>
         </div>
     )
