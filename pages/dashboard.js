4558d9a0243d37f51ece5abda33264d9d0d34f42
Kyle Canamar <canamar.kyle@gmail.com>
2022-08-09 21:30:27 -0700

    renders all maps own by logged in user

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,15 +1,29 @@
 import Layout from '../components/Layout'
-import Content from '../components/Content'
+import client from "../lib/sanity/client"
+import useAuth from '../hooks/useAuth'
 import { useState, useEffect, createContext } from "react"
+import MapList from '../components/map/List'
 
 export const AppContext = createContext()
 
 export default function Dashboard() {
+  const { user, loading } = useAuth()
+  const [mapList, setMapList] = useState([])
 
+  const fetchMaps = async () => {
+    let fetchedMaps;
+    if (!loading) {
+      fetchedMaps = await client.fetch(`*[_type=='map' && user==$user] | {_id, title, start, end, user}`,{ user: user.email})
+      setMapList(fetchedMaps)
+    }
+  }
+
+  useEffect(() => {fetchMaps()}, [loading, user])
+  
   return (
     <AppContext.Provider>
         <Layout home>
-            <Content/>
+          <MapList user={user} mapList={mapList}/>
         </Layout>
     </AppContext.Provider>
   )
