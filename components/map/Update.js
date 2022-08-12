e3fa0ead712da056f22ad153484a36512affab2f
Kyle Canamar <canamar.kyle@gmail.com>
2022-08-11 19:03:07 -0700

    successful render of updated content

--- components/map/Update.js
+++ components/map/Update.js
@@ -1,8 +1,10 @@
 import { useState } from "react"
+import { useRouter } from "next/router"
 
 export default function UpdateMap({ map }) {
     const [title, setTitle] = useState(map.title)
     const [errMessage, setErrMessage] = useState("")
+    const router = useRouter()
 
     const titleChange = async (e) => {
         e.preventDefault()
@@ -26,31 +28,30 @@ export default function UpdateMap({ map }) {
                     title: title,
                 })
             })
+
+            router.reload()
         }
     }
 
     return (
-        <div className="text-center">
-            <h1>Update Map</h1>
-            <form>
+        <div className="text-center mx-4 p-5">
+            <h1 className="text-2xl font-bold">Update your map title below</h1>
+            <form className="mt-2">
               <div className="flex justify-center items-center">
-                <label htmlFor="map" className="invisible">Name of Map</label>
                 <input
-                  className="w-72 h-12 border p-4 border-blue-100"
+                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-nonedark:border-gray-600 focus:outline-none focus:ring-0 focus:border-blue-600"
                   type="text"
                   value={title}
                   placeholder="Quarter 1"
                   onChange={titleChange}
                   name="title"
                 />{" "}
-              </div>{" "}
+              </div>
               <button
-                className="focus:outline-none focus:ring focus:border-blue-800
-                                    px-6 py-2 rounded-xl bg-blue-500 text-blue-50 hover:bg-blue-800 
-                                    font-semibold"
+                className="text-white mt-2 bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                 onClick={handleSubmit}
               >
-                Submit
+                Update
               </button>
               <p>{errMessage}</p>
             </form>
